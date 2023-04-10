b04167455201babe2b49b09f47d8e13f3d8eff2e
XibalbaM <maelporret@outlook.fr>
2023-04-10 21:41:58 +0200

    Front base 1

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,26 +1,50 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+const {add} = require("karma-coverage/lib/coverage-map");
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
-      colors: {
-        "purple": "#EE88FF",
-        "yellow": "#FFCD3C",
-        "blue": "#3CFFD0",
-        "light-gray": "#A3A5A2",
-        "gray": "#6A6A6A",
-        "dark-gray": "#424242",
-        "orange": "#FF631E",
-        "light": "#DFDFDF",
-        "white": "#FFFFFF",
-        "lighter-dark": "#2F2F2F",
-        "light-dark": "#1F1F1F",
-        "dark": "#121212",
-        "black": "#030505"
-      }
+    colors: {
+      "transparent": "transparent",
+      "current": "currentColor",
+      "purple": "#EE88FF",
+      "yellow": "#FFCD3C",
+      "orange": "#FF631E",
+      "blue": "#3CFFD0",
+      "white": "#FFFFFF",
+      "light": "#DFDFDF",
+      "light-gray": "#A3A5A2",
+      "gray": "#6A6A6A",
+      "dark-gray": "#424242",
+      "lighter-dark": "#2F2F2F",
+      "light-dark": "#1F1F1F",
+      "dark": "#121212",
+      "black": "#030505"
     },
+    fontFamily: {
+      "monument": ['PP Monument Extended', 'sans-serif'],
+      "neue": ['PP Neue Montreal', 'sans-serif'],
+    },
+    fontWeight: {
+      normal: '450',
+      bold: '675',
+    }
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant, addUtilities }) {
+      addVariant('hocus', ['&:hover', '&:focus']);
+
+      // Scrollbars
+      addVariant('scrollbar', '&::-webkit-scrollbar');
+      addVariant('scrollbar-thumb', '&::-webkit-scrollbar-thumb');
+      addVariant('scrollbar-track', '&::-webkit-scrollbar-track');
+      addVariant('scrollbar-track-piece', '&::-webkit-scrollbar-track-piece');
+      addVariant('scrollbar-corner', '&::-webkit-scrollbar-corner');
+      addVariant('scrollbar-button', '&::-webkit-scrollbar-button');
+      addVariant('resizer', '&::-webkit-resizer');
+    })
+  ],
 }
