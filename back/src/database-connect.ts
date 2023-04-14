2a390556452f9908c1061e9905f99d075b20f44e
XibalbaM <maelporret@outlook.fr>
2023-04-14 22:38:21 +0200

    Ajout d'un paramètre pour le nom de la bdd pour pouvoir avoir une db différente pour test et pour coder.

--- back/src/database-connect.ts
+++ back/src/database-connect.ts
@@ -4,7 +4,7 @@ import config from "./config/env.js";
 
 console.log(`Connecting to database...`);
 
-mongoose.connect(config.db);
+mongoose.connect(config.db, {dbName: config.dbName});
 mongoose.connection.on("error", (error) => {
     throw new Error(`unable to connect to database: ${error}`);
 });
@@ -12,7 +12,7 @@ mongoose.connection.on("connected", () => {
 
     console.log(`Connected to database`);
 
-    if (config.env === "test") {
+    if (config.env === "test" || config.env === "development") {
         mongoose.set("debug", true);
     }
 });
