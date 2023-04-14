c07fc4b7d97062a9657e06b1308d60f43a9d0009
XibalbaM <maelporret@outlook.fr>
2023-04-14 22:34:56 +0200

    Ajout du support pour le token dans les utilitaires de test.

--- back/src/utils/tests.utils.ts
+++ back/src/utils/tests.utils.ts
@@ -12,23 +12,32 @@ function processUrl(url: string) {
 /**
  * returns the response of a GET request to the given url.
  * @param url the url to send the request to.
+ * @param token the token to use for authentication.
  * @returns a promise that resolves to the response.
  */
-export function get(url: string): Promise<Response> {
-    return fetch(processUrl(url));
+export function get(url: string, token?: string): Promise<Response> {
+    return fetch(processUrl(url), {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
 }
 
 /**
  * returns the response of a POST request to the given url.
  * @param url the url to send the request to.
  * @param body the body of the request.
+ * @param token the token to use for authentication.
  * @returns a promise that resolves to the response.
  */
-export function post(url: string, body: any): Promise<Response> {
+export function post(url: string, body: any, token?: string): Promise<Response> {
     return fetch(processUrl(url), {
         method: 'POST',
         body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
     });
 }
 
@@ -36,23 +45,31 @@ export function post(url: string, body: any): Promise<Response> {
  * returns the response of a PUT request to the given url.
  * @param url the url to send the request to.
  * @param body the body of the request.
+ * @param token the token to use for authentication.
  * @returns a promise that resolves to the response.
  */
-export function put(url: string, body: any): Promise<Response> {
+export function put(url: string, body: any, token?: string): Promise<Response> {
     return fetch(processUrl(url), {
         method: 'PUT',
         body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
     });
 }
 
 /**
  * returns the response of a DELETE request to the given url.
  * @param url the url to send the request to.
+ * @param token the token to use for authentication.
  * @returns a promise that resolves to the response.
  */
-export function del(url: string): Promise<Response> {
+export function del(url: string, token?: string): Promise<Response> {
     return fetch(processUrl(url), {
-        method: 'DELETE'
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
     });
 }
