150f7673dd63964fc3befe085e68814e1c426a2b
XibalbaM <maelporret@outlook.fr>
2023-04-17 19:20:18 +0200

    Changing vitest.config.js to avoid automatic test stat on file change.

--- back/vitest.config.js
+++ back/vitest.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		dir: 'src/tests',
+		watchExclude: ['./**/*'] //To avoid starting the test runner when a file is changed, because the tests fails because the server is not fully started
 	},
 });
