3d3d97005cadb61895f00cb1aaed8cb33ce66ecd
XibalbaM <maelporret@outlook.fr>
2023-04-23 14:18:17 +0200

    Applying bg conditionally depending on the page

--- front/src/app/app/app.component.ts
+++ front/src/app/app/app.component.ts
@@ -2,13 +2,18 @@ import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'naruto-jdr-online-builder';
+  title = "naruto-jdr-online-builder";
   showNav: boolean = true;
+  imageBg: boolean = false;
+  private readonly imageBgPages: string[] = [
+    "/connexion",
+    "/",
+  ];
 
   constructor(private router: Router) {
   }
@@ -16,6 +21,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.router.events.subscribe(() => {
       this.showNav = !this.router.url.includes("/connexion");
+      this.imageBg = this.imageBgPages.includes(this.router.url);
     });
   }
 }
