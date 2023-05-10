0fe0030616c984bd93b85ff18aa7f42fce148f2f
XibalbaM <maelporret@outlook.fr>
2023-05-10 21:43:52 +0200

    feat(front)!: Make app accessible only once logged.
    Add auth guard to every routes expect connexion routes, to force connection.
    
    BREAKING-CHANGE: The user MUST be authenticated to access the different pages

--- front/src/app/app/app-routing.module.ts
+++ front/src/app/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {HomeComponent} from "./components/home/home.component";
 import {AuthenticatedGuard} from "./guards/authenticated.guard";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent, canActivate: [AuthenticatedGuard]},
   { path: 'connexion', loadChildren: () => import('../login/login.module').then(m => m.LoginModule) },
   { path: 'compte', loadChildren: () => import('../account/account.module').then(m => m.AccountModule), canActivate: [AuthenticatedGuard] },
 ];
