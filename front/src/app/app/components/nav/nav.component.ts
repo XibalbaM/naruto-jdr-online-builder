37ceef50f3b39f9afdaaae2b54461f515b5480fb
XibalbaM <maelporret@outlook.fr>
2023-05-10 14:23:52 +0200

    feat(front): Allow to return to the home page using the navbar when on one of the pages on it

--- front/src/app/app/components/nav/nav.component.ts
+++ front/src/app/app/components/nav/nav.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import Auth from "../../models/auth.model";
 import {Router} from "@angular/router";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
-  constructor(public auth: Auth, public router: Router) { }
+  accountUrl: BehaviorSubject<string> = new BehaviorSubject<string>('/connexion');
+  onAccount: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  onList: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  onCreate: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
+  constructor(public auth: Auth, private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((val) => {
+      this.onCreate.next(this.router.url.startsWith('/creation'));
+      this.onList.next(this.router.url.startsWith('/liste'));
+      this.onAccount.next(this.router.url.startsWith(this.accountUrl.getValue()));
+    });
+    this.auth.tokenObservable().subscribe((token) => {
+      if (token) {
+        this.accountUrl.next("/compte");
+      } else {
+        this.accountUrl.next("/connexion");
+      }
+    });
+  }
 }
