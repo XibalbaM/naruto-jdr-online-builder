d67651e4cb389e3a71b188c95a5f432a73c3e6b5
XibalbaM <maelporret@outlook.fr>
2023-05-13 21:24:24 +0200

    style(front): Reformat account.module.ts

--- front/src/app/account/account.module.ts
+++ front/src/app/account/account.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
 
-import { AccountRoutingModule } from './account-routing.module';
-import { EditComponent } from './components/edit/edit.component';
+import {AccountRoutingModule} from "./account-routing.module";
+import {EditComponent} from "./components/edit/edit.component";
 import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    EditComponent
+    EditComponent,
   ],
-    imports: [
-        CommonModule,
-        AccountRoutingModule,
-        SharedModule,
-    ],
+  imports: [
+    CommonModule,
+    AccountRoutingModule,
+    SharedModule,
+  ],
 })
-export class AccountModule { }
+export class AccountModule {
+}
