eab397874f52d3750f15de105dd1706f7645d391
XibalbaM <maelporret@outlook.fr>
2023-05-16 20:35:22 +0200

    refactor(bot): Add containing class for api.utils.ts

--- bot/src/utils/api.utils.ts
+++ bot/src/utils/api.utils.ts
@@ -4,77 +4,86 @@ import config from "../config/env.js";
 import axios, {AxiosResponse} from "axios";
 
 /**
- * Processes the given url to make sure it is a full url and to provide a shorthand for api calls.
- * @param url the url to process.
- * @returns the processed url.
+ * A class containing utility methods for api calls.
  */
-function processUrl(url: string) {
-    if (url.startsWith("/")) {
-        return config.api_url + url;
+export default class ApiUtils {
+    /**
+     * Processes the given url to make sure it is a full url and to provide a shorthand for api calls.
+     * @param url the url to process.
+     * @returns the processed url.
+     */
+    static processUrl(url: string) {
+        if (url.startsWith("/")) {
+            return config.api_url + url;
+        }
+        return url;
     }
-    return url;
-}
 
-export function tokenFromId(id: string | undefined) {
-    return id ? jwt.sign({discordId: id}, config.jwt_secret, {expiresIn: config.jwt_expiration}) : undefined;
-}
+    static tokenFromId(id: string | undefined) {
+        return id ? jwt.sign({discordId: id}, config.jwt_secret, {expiresIn: config.jwt_expiration}) : undefined;
+    }
 
-/**
- * Returns the response of a GET request to the given url.
- * @param url the url to send the request to.
- * @param id the id of the user making the request.
- * @returns a promise that resolves to the response.
- */
-export function get<T>(url: string, id?: string): Promise<AxiosResponse<T>> {
-    return axios.get<T>(processUrl(url), {
-        headers: {
-            "Authorization": tokenFromId(id) ? `Bearer ${tokenFromId(id)}` : "",
-        },
-    });
-}
+    /**
+     * Returns the response of a GET request to the given url.
+     * @param url the url to send the request to.
+     * @param id the id of the user making the request.
+     * @returns a promise that resolves to the response.
+     */
+    static get<T>(url: string, id?: string): Promise<AxiosResponse<T>> {
+        return axios.get<T>(this.processUrl(url), {
+            validateStatus: (status) => true,
+            headers: {
+                "Authorization": this.tokenFromId(id) ? `Bearer ${this.tokenFromId(id)}` : "",
+            },
+        });
+    }
 
-/**
- * Returns the response of a POST request to the given url.
- * @param url the url to send the request to.
- * @param body the body of the request.
- * @param id the id of the user making the request.
- * @returns a promise that resolves to the response.
- */
-export function post<T>(url: string, body: any, id?: string): Promise<AxiosResponse<T>> {
-    return axios.post<T>(processUrl(url), body, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": tokenFromId(id) ? `Bearer ${tokenFromId(id)}` : "",
-        },
-    });
-}
+    /**
+     * Returns the response of a POST request to the given url.
+     * @param url the url to send the request to.
+     * @param body the body of the request.
+     * @param id the id of the user making the request.
+     * @returns a promise that resolves to the response.
+     */
+    static post<T>(url: string, body: any, id?: string): Promise<AxiosResponse<T>> {
+        return axios.post<T>(this.processUrl(url), body, {
+            validateStatus: (status) => true,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": this.tokenFromId(id) ? `Bearer ${this.tokenFromId(id)}` : "",
+            },
+        });
+    }
 
-/**
- * Returns the response of a PUT request to the given url.
- * @param url the url to send the request to.
- * @param body the body of the request.
- * @param id the id of the user making the request.
- * @returns a promise that resolves to the response.
- */
-export function put<T>(url: string, body: any, id?: string): Promise<AxiosResponse<T>> {
-    return axios.put<T>(processUrl(url), body, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": tokenFromId(id) ? `Bearer ${tokenFromId(id)}` : "",
-        },
-    });
-}
+    /**
+     * Returns the response of a PUT request to the given url.
+     * @param url the url to send the request to.
+     * @param body the body of the request.
+     * @param id the id of the user making the request.
+     * @returns a promise that resolves to the response.
+     */
+    static put<T>(url: string, body: any, id?: string): Promise<AxiosResponse<T>> {
+        return axios.put<T>(this.processUrl(url), body, {
+            validateStatus: (status) => true,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": this.tokenFromId(id) ? `Bearer ${this.tokenFromId(id)}` : "",
+            },
+        });
+    }
 
-/**
- * Returns the response of a DELETE request to the given url.
- * @param url the url to send the request to.
- * @param id the id of the user making the request.
- * @returns a promise that resolves to the response.
- */
-export function del<T>(url: string, id?: string): Promise<AxiosResponse<T>> {
-    return axios.delete<T>(processUrl(url), {
-        headers: {
-            "Authorization": tokenFromId(id) ? `Bearer ${tokenFromId(id)}` : "",
-        },
-    });
+    /**
+     * Returns the response of a DELETE request to the given url.
+     * @param url the url to send the request to.
+     * @param id the id of the user making the request.
+     * @returns a promise that resolves to the response.
+     */
+    static del<T>(url: string, id?: string): Promise<AxiosResponse<T>> {
+        return axios.delete<T>(this.processUrl(url), {
+            validateStatus: (status) => true,
+            headers: {
+                "Authorization": this.tokenFromId(id) ? `Bearer ${this.tokenFromId(id)}` : "",
+            },
+        });
+    }
 }
