be249ed635b247fbdbca4b0490abef3df2e7ddaa
XibalbaM <maelporret@outlook.fr>
2023-06-04 20:27:30 +0200

    fix(front): Replace check of service token with check of localstorage token, because the service initialize after the gard.

--- front/src/app/app/guards/authenticated.guard.ts
+++ front/src/app/app/guards/authenticated.guard.ts
@@ -12,10 +12,10 @@ import Auth from "../models/auth.model";
  */
 export class AuthenticatedGuard implements CanActivate {
 
-  constructor(private auth: Auth, private router: Router) {}
+  constructor(private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.auth.token) {
+    if (localStorage.getItem('token')) {
       return true;
     } else {
       this.router.navigateByUrl('/connexion');
