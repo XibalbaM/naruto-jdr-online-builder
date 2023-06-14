45d54dd2c7afc2f772534322b686c29b538cef75
XibalbaM <maelporret@outlook.fr>
2023-06-14 21:48:15 +0200

    fix(front): Remove guard loop

--- front/src/app/app/app-routing.module.ts
+++ front/src/app/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {AuthenticatedGuard} from "./guards/authenticated.guard";
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthenticatedGuard]},
-  { path: 'connexion', loadChildren: () => import('../login/login.module').then(m => m.LoginModule), canActivate: [AuthenticatedGuard] },
+  { path: 'connexion', loadChildren: () => import('../login/login.module').then(m => m.LoginModule) },
   { path: 'compte', loadChildren: () => import('../account/account.module').then(m => m.AccountModule), canActivate: [AuthenticatedGuard] },
   { path: 'personnages', loadChildren: () => import('../characters/characters.module').then(m => m.CharactersModule), canActivate: [AuthenticatedGuard] },
 ];
