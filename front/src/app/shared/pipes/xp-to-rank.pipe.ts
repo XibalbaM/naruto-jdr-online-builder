898f4387b36d6dcfbd08ab1c8829c884369c82b3
XibalbaM <maelporret@outlook.fr>
2023-06-20 19:41:51 +0200

    fix(front): Replace use of img by a background in spacer-graphical.component.html

---
+++ front/src/app/shared/pipes/xp-to-rank.pipe.ts
@@ -0,0 +1,21 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import Rank from "../../app/models/rank.model";
+import {DataService} from "../../app/services/data.service";
+
+@Pipe({
+    name: 'xpToRank'
+})
+export class XpToRankPipe implements PipeTransform {
+
+    constructor(private dataService: DataService) {
+    }
+
+    transform(xp: number): Rank {
+        for (const rank of this.dataService.ranks.getValue().reverse()) {
+            if (rank.minXp < xp) {
+                return rank;
+            }
+        }
+        return this.dataService.ranks.getValue()[0];
+    }
+}
--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -1,34 +1,39 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import { SpacerGraphicalComponent } from './components/spacer-graphical/spacer-graphical.component';
-import { SpacerComponent } from './components/spacer/spacer.component';
+import {SpacerGraphicalComponent} from './components/spacer-graphical/spacer-graphical.component';
+import {SpacerComponent} from './components/spacer/spacer.component';
 import {RolesPipe} from "./pipes/roles.pipe";
-import { RoleNamePipe } from './pipes/role-name.pipe';
+import {RoleNamePipe} from './pipes/role-name.pipe';
+import {XpToRankPipe} from './pipes/xp-to-rank.pipe';
 
 @NgModule({
-  declarations: [
-    SpacerGraphicalComponent,
-    SpacerComponent,
-    RolesPipe,
-    RoleNamePipe
-  ],
-  imports: [
-    CommonModule,
-    NgOptimizedImage,
-    FormsModule,
-  ],
-  exports: [
-    CommonModule,
-    NgOptimizedImage,
-    FormsModule,
-    SpacerGraphicalComponent,
-    SpacerComponent,
-    RolesPipe,
-    RoleNamePipe,
-  ],
-  providers: [
-    RoleNamePipe
-  ]
+    declarations: [
+        SpacerGraphicalComponent,
+        SpacerComponent,
+        RolesPipe,
+        RoleNamePipe,
+        XpToRankPipe
+    ],
+    imports: [
+        CommonModule,
+        NgOptimizedImage,
+        FormsModule,
+    ],
+    exports: [
+        CommonModule,
+        NgOptimizedImage,
+        FormsModule,
+        SpacerGraphicalComponent,
+        SpacerComponent,
+        RolesPipe,
+        RoleNamePipe,
+        XpToRankPipe,
+    ],
+    providers: [
+        RoleNamePipe,
+        XpToRankPipe
+    ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
