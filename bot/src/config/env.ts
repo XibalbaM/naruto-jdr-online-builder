09ee4d8d0d928a39d83c1cefcd44e1b01f2ec710
XibalbaM <maelporret@outlook.fr>
2023-06-21 19:43:52 +0200

    fix(bot): Fix small error in env files names

--- bot/src/config/env.ts
+++ bot/src/config/env.ts
@@ -2,7 +2,7 @@ import Environment from "./env/Environment";
 
 const env = process.env.NODE_ENV || "test";
 console.log(`Loading ${env} environment...`);
-let importPath = `./env/${env}.js`;
+let importPath = `./env/${env}.env.js`;
 if (env === "test") {
     importPath = importPath.replace(".js", ".ts");
 }
