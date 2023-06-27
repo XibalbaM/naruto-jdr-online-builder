1bec9ec6d0ddc16a562a506cb2771e8118734831
XibalbaM <maelporret@outlook.fr>
2023-06-27 17:00:38 +0200

    feat(bot): Add help command

---
+++ bot/src/commands/help.command.ts
@@ -0,0 +1,20 @@
+import {ComponentType, SlashCommandBuilder} from "discord.js";
+import Responses from "../utils/responses.utils.js";
+import {SlashCommand} from "../classes.js";
+
+const command: SlashCommand = {
+    command: new SlashCommandBuilder()
+        .setName("aide")
+        .setDescription("Affiche la liste des commandes"),
+    async execute(interaction) {
+
+        const text = "- " + interaction.client.slashCommands
+            .filter(value => value.implemented === undefined || value.implemented)
+            .map((value) => `**/${value.command.name}**\n${value.command.description}`)
+            .join("\n- ");
+
+        await Responses.sendHelp(interaction, text);
+    }
+};
+
+export default command;
