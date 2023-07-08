b7e307d5fc77afa007afc9540964bef102091477
XibalbaM <maelporret@outlook.fr>
2023-07-08 12:25:09 +0200

    fix(bot): Fix ready text

--- bot/src/events/ready.event.ts
+++ bot/src/events/ready.event.ts
@@ -5,7 +5,7 @@ const event: BotEvent = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(`Logged in as ${client.user.tag}! (https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=274878024704&scope=bot%20applications.commands)`);
+        console.log(`Logged in as ${client.user.tag} ! (https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=274878024704&scope=bot%20applications.commands)`);
     },
 };
 
