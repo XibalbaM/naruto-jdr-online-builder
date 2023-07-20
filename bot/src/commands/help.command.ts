2e95012bd66001b0c152462031844757668aa037
XibalbaM <maelporret@outlook.fr>
2023-07-20 17:09:10 +0200

    style(bot): Reformat help.command.ts

--- bot/src/commands/help.command.ts
+++ bot/src/commands/help.command.ts
@@ -3,19 +3,19 @@ import Responses from "../utils/responses.utils.js";
 import {SlashCommand} from "../classes.js";
 
 const command: SlashCommand = {
-    command: new SlashCommandBuilder()
-        .setName("aide")
-        .setDescription("Affiche la liste des commandes"),
-    async execute(interaction) {
+	command: new SlashCommandBuilder()
+		.setName("aide")
+		.setDescription("Affiche la liste des commandes"),
+	async execute(interaction) {
 
-        const text = "- " + interaction.client.slashCommands
-            .filter(value => value.implemented === undefined || value.implemented)
-            .sort((a, b) => a.command.name.localeCompare(b.command.name))
-            .map((value) => `**/${value.command.name}**\n${value.command.description}`)
-            .join("\n- ");
+		const text = "- " + interaction.client.slashCommands
+			.filter(value => value.implemented === undefined || value.implemented)
+			.sort((a, b) => a.command.name.localeCompare(b.command.name))
+			.map((value) => `**/${value.command.name}**\n${value.command.description}`)
+			.join("\n- ");
 
-        await Responses.successEmbed(interaction, "Liste des commandes", text);
-    }
+		await Responses.successEmbed(interaction, "Liste des commandes", text);
+	}
 };
 
 export default command;
