d4f6e0507abd69d30df45f4cf561c6fa76b9e6ce
XibalbaM <maelporret@outlook.fr>
2023-07-20 13:33:33 +0200

    fix(bot): Remove guildId because it became useless

--- bot/src/config/env/Environment.ts
+++ bot/src/config/env/Environment.ts
@@ -2,7 +2,6 @@ export default class Environment {
     env!: string;
     token!: string;
     clientId!: string;
-    guildId!: string;
     api_url!: string;
     jwt_secret!: string;
     jwt_expiration!: number;
