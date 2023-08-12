4f6e2aae8585432d19dca68864d8dae4b47fbf90
XibalbaM <maelporret@outlook.fr>
2023-08-13 01:32:57 +0200

    feat(front): Add a var to disallow spamming on submit button in connexion view

--- front/src/app/login/components/input/input.component.ts
+++ front/src/app/login/components/input/input.component.ts
@@ -5,33 +5,42 @@ import {ReCaptchaV3Service} from "ngx-captcha";
 import Environment from "../../../../environments/environment.interface";
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+    selector: 'app-input',
+    templateUrl: './input.component.html',
+    styleUrls: ['./input.component.scss']
 })
 /**
- * The component where th user can log in or create his account.
+ * The component where the user can log in or create his account.
  */
 export class InputComponent {
 
-  userEmail?: string;
+    userEmail?: string;
+    sent = false;
 
-  constructor(private router: Router, private authService: AuthService, private recaptchaV3Service: ReCaptchaV3Service, private environment: Environment) {}
-
-  /**
-   * Once the form is submitted, call the api to send the connection email then send the user to the callback view.
-   *
-   * @see CallbackComponent
-   */
-  onSubmit() {
-    if (this.userEmail) {
-      this.recaptchaV3Service.execute(this.environment.recaptchaSiteKey, 'login', (token) => {
-        this.authService.sendEmailRequest(this.userEmail!, token).subscribe((response) => {
-          this.router.navigate(['/connexion/reponse'], {queryParams: {isRegistration: response.isRegistration, error: response.error, email: this.userEmail}});
-        });
-      }, {}, (error) => {
-        console.error(error)
-      });
+    constructor(private router: Router, private authService: AuthService, private recaptchaV3Service: ReCaptchaV3Service, private environment: Environment) {
     }
-  }
+
+    /**
+     * Once the form is submitted, call the api to send the connection email then send the user to the callback view.
+     *
+     * @see CallbackComponent
+     */
+    onSubmit() {
+        if (this.userEmail && !this.sent) {
+            this.recaptchaV3Service.execute(this.environment.recaptchaSiteKey, 'login', (token) => {
+                this.sent = true;
+                this.authService.sendEmailRequest(this.userEmail!, token).subscribe((response) => {
+                    this.router.navigate(['/connexion/reponse'], {
+                        queryParams: {
+                            isRegistration: response.isRegistration,
+                            error: response.error,
+                            email: this.userEmail
+                        }
+                    });
+                });
+            }, {}, (error) => {
+                console.error(error)
+            });
+        }
+    }
 }
