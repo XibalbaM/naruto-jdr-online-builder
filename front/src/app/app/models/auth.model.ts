a526ad4704dde8bed84e1c7c06bcb0ed98070d02
XibalbaM <maelporret@outlook.fr>
2023-08-13 01:37:03 +0200

    feat(front): Add observables in auth.model.ts witch only emit when value exist (is not undefined). It's used to make subscriptions to these observables wait for services loading before run

--- front/src/app/app/models/auth.model.ts
+++ front/src/app/app/models/auth.model.ts
@@ -1,52 +1,75 @@
 import User from "./user.model";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, filter, Observable} from "rxjs";
 
 /**
  * Class that contains the authentication data.
  * @class Auth
  */
 export default class Auth {
-  /**
-   * The token of the user, as a JWT, if the user is connected.
-   *
-   * Takes the form of a BehaviorSubject to be able to be observed.
-   * @private
-   */
-  private _token = new BehaviorSubject<string | undefined>(undefined);
-  /**
-   * The user data, if the user is connected.
-   *
-   * Takes the form of a BehaviorSubject to be able to be observed.
-   * @see User
-   * @private
-   */
-  private _user = new BehaviorSubject<User | undefined>(undefined);
+    /**
+     * The token of the user, as a JWT, if the user is connected.
+     *
+     * Takes the form of a BehaviorSubject to be able to be observed.
+     * @private
+     */
+    private _token = new BehaviorSubject<string | undefined>(undefined);
+    /**
+     * The user data, if the user is connected.
+     *
+     * Takes the form of a BehaviorSubject to be able to be observed.
+     * @see User
+     * @private
+     */
+    private _user = new BehaviorSubject<User | undefined>(undefined);
 
-  get user(): User | undefined {
-    return this._user.getValue();
-  }
+    get user(): User | undefined {
+        return this._user.getValue();
+    }
 
-  set user(user: User | undefined) {
-    this._user.next(user);
-  }
+    set user(user: User | undefined) {
+        this._user.next(user);
+    }
 
-  userObservable(): BehaviorSubject<User | undefined> {
-    return this._user;
-  }
+    userObservable(): BehaviorSubject<User | undefined> {
+        return this._user;
+    }
 
-  refreshUser() {
-    this.tokenObservable().next(this.token);
-  }
+    /**
+     * Returns an observable that will emit the user data only if it exists.
+     * Should be used in authenticated routes to wait for the service to load
+     * before leaving the page.
+     *
+     * Example: NgOnInit needs the list of characters, but the service is not loaded yet.
+     * So, we use this function to wait for the service to load
+     */
+    userObservableOnceLoaded(): Observable<User> {
+        return this._user.pipe(
+            filter((user): user is User => user !== undefined)
+        );
+    }
 
-  get token(): string | undefined {
-    return this._token.getValue();
-  }
+    refreshUser() {
+        this.tokenObservable().next(this.token);
+    }
 
-  set token(token: string | undefined) {
-    this._token.next(token);
-  }
+    get token(): string | undefined {
+        return this._token.getValue();
+    }
 
-  tokenObservable(): BehaviorSubject<string | undefined> {
-    return this._token;
-  }
+    set token(token: string | undefined) {
+        this._token.next(token);
+    }
+
+    tokenObservable(): BehaviorSubject<string | undefined> {
+        return this._token;
+    }
+
+    /**
+     * Same as {@link userObservableOnceLoaded}, but for the token
+     */
+    tokenObservableOnceLoaded(): Observable<string> {
+        return this._token.pipe(
+            filter((token): token is string => token !== undefined)
+        );
+    }
 }
