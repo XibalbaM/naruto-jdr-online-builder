8bfe64dcb8a9448de1bfdf2aee7638a95774394d
XibalbaM <maelporret@outlook.fr>
2023-08-13 01:41:06 +0200

    feat(front): Add group model

---
+++ front/src/app/app/models/group.model.ts
@@ -0,0 +1,12 @@
+/**
+ * Class representing a Group.
+ * @class Group
+ */
+export default class Group {
+	_id!: number;
+	name!: string;
+
+	constructor(group: Group) {
+		Object.assign(this, group);
+	}
+}
