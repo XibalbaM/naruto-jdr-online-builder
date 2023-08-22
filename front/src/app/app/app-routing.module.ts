999426dab4052b82baa7c4643027f9ef9faa0c9f
XibalbaM <maelporret@outlook.fr>
2023-08-22 18:36:13 +0200

    feat(front): Add navbar, bg and title spec on all routes

--- front/src/app/app/app-routing.module.ts
+++ front/src/app/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import {HomeComponent} from "./components/home/home.component";
 import {AuthenticatedGuard} from "./guards/authenticated.guard";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthenticatedGuard]},
-  { path: 'connexion', loadChildren: () => import('../login/login.module').then(m => m.LoginModule) },
+  { path: '', component: HomeComponent, canActivate: [AuthenticatedGuard], data: {bgMethode: "image"}, title: "Naruto jdr"},
+  { path: 'connexion', loadChildren: () => import('../login/login.module').then(m => m.LoginModule), data: {navbar: "none", bgMethode: "image"}, title: "Connexion — Naruto jdr"},
   { path: 'compte', loadChildren: () => import('../account/account.module').then(m => m.AccountModule), canActivate: [AuthenticatedGuard] },
   { path: 'personnages', loadChildren: () => import('../characters/characters.module').then(m => m.CharactersModule), canActivate: [AuthenticatedGuard] },
 ];
