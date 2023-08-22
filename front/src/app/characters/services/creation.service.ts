be37334be301e932d5b699bcc3a7f790c8fadde7
XibalbaM <maelporret@outlook.fr>
2023-08-22 18:29:05 +0200

    feat(front): Finish implementation of creation.service.ts, witch is now fully functional

--- front/src/app/characters/services/creation.service.ts
+++ front/src/app/characters/services/creation.service.ts
@@ -3,71 +3,111 @@ import Character from "../../app/models/character.model";
 import Village from "../../app/models/village.model";
 import Clan from "../../app/models/clan.model";
 import Road from "../../app/models/road.model";
-import Skill from "../../app/models/skill.model";
-import Base from "../../app/models/base.model";
 import {DataService} from "../../app/services/data.service";
 import {XpToRankPipe} from "../../shared/pipes/xp-to-rank.pipe";
+import {catchError, map, mergeMap, Observable, of, tap, zip} from "rxjs";
+import {ApiService} from "../../app/services/api.service";
+import Auth from "../../app/models/auth.model";
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class CreationService {
 
-    character: Character = new Character();
-    step: number = 1;
+	character: Character = new Character();
+	step: number = 1;
 
-    constructor(private dataService: DataService, private rankPipe: XpToRankPipe) {
-    }
+	constructor(private dataService: DataService, private rankPipe: XpToRankPipe, private apiService: ApiService, private auth: Auth) {
+	}
 
-    /**
-     * Get data from the first step of the creation process.
-     * @param village The village of the character.
-     * @param firstName The first name of the character.
-     * @param clan The clan of the character.
-     * @param xp The xp of the character.
-     * @param road The road of the character.
-     */
-    stepOne(village: Village, firstName: string, clan: Clan, xp: number, road?: Road) {
+	/**
+	 * Get data from the first step of the creation process.
+	 * @param village The village of the character.
+	 * @param firstName The first name of the character.
+	 * @param clan The clan of the character.
+	 * @param xp The xp of the character.
+	 * @param road The road of the character.
+	 */
+	stepOne(village: Village, firstName: string, clan: Clan, xp: number, road?: Road) {
 
-        this.character.village = village;
-        this.character.firstName = firstName;
-        this.character.clan = clan;
-        this.character.rank = this.rankPipe.transform(xp);
-        this.character.xp = xp;
-        this.character.road = road;
+		this.character.village = village._id;
+		this.character.firstName = firstName;
+		this.character.clan = clan._id;
+		this.character.xp = xp;
+        this.character.rank = this.rankPipe.transform(xp)._id;
+		this.character.road = road?._id;
 
-        this.step = 2;
-    }
+		this.step = 2;
+	}
 
-    /**
-     * Get data from the second step of the creation process.
-     * @param nindo The nindo of the character.
-     * @param story The story of the character.
-     */
-    stepTwo(nindo: string, story: string) {
+	/**
+	 * Get data from the second step of the creation process.
+	 * @param nindo The nindo of the character.
+	 * @param story The story of the character.
+	 */
+	stepTwo(nindo: string, story?: string) {
 
-        this.character.nindo = nindo;
-        this.character.story = story;
+		this.character.nindo = nindo;
+		this.character.notes = story || "";
 
-        this.step = 3;
-    }
+		this.step = 3;
+	}
 
-    /**
-     * Get data from the third step of the creation process.
-     * @param skills The skills of the character, including the clan skills.
-     */
-    stepThree(skills: Skill[]) {
-        this.character.skills = new Map<Skill, number>();
+	/**
+	 * Get data from the third step of the creation process.
+	 * @param skillIds The skills of the character, including the clan skills.
+	 */
+	stepThree(skillIds: string[]): Observable<{success: boolean, id?: string}> {
 
-        for (let skill of skills) {
-            this.character.skills.set(skill, 1);
-        }
-
-        this.step = 1;
-        this.character.bases = new Map<Base, number>();
-        this.dataService.bases.getValue().forEach(base => {
-            this.character.bases.set(base, 1);
-        })
-        //TODO: Send the character to the API.
-    }
+		return this.apiService.doRequest<{ character: Character }>("POST", "/characters", {character: new Character(this.character).toCreate()}).pipe(
+			map((response) => {
+				if (response.status !== 201 || !response.body || !response.body.character) {
+					console.log(response);
+					throw new Error("Error while creating the character.");
+				} else {
+					return response.body.character;
+				}
+			}),
+			mergeMap((character) => {
+				return zip([
+					of(character),
+					...skillIds.map((skillId) => this.apiService.doRequest("POST", "/characters/" + character._id + "/skills/" + skillId, {value: 1}).pipe(map((response) => {
+						console.log(response);
+						return response.status === 200;
+					})))
+				]);
+			}),
+			map((results) => {
+				if (results.includes(false)) {
+					throw new Error("Error while creating the character.");
+				} else {
+					return results[0];
+				}
+			}),
+            map((character) => {
+                skillIds.forEach((skillId) => {
+                    character.skills.find((data) => data.skill === skillId)!.level = 1;
+                });
+                return character;
+            }),
+			tap((character) => {
+				this.character = new Character();
+				this.step = 1;
+				if (this.auth.user) {
+					this.auth.user = {
+						...this.auth.user,
+						characters: [
+							...this.auth.user.characters,
+							character
+						]
+					}
+				}
+			}),
+			map((character) => ({success: true, id: character._id})),
+			catchError((error) => {
+				console.error(error);
+				return of({success: false});
+			})
+		);
+	}
 }
