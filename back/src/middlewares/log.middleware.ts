c132c47bc2b5d7221026217153281fbd73d31da4
XibalbaM <maelporret@outlook.fr>
2023-08-29 21:27:54 +0200

    feat(back): Adapted log.middleware.ts to work with cookies

--- back/src/middlewares/log.middleware.ts
+++ back/src/middlewares/log.middleware.ts
@@ -14,14 +14,19 @@ export default function (basePath: string): Middleware {
         if (req.headers.authorization) {
             try {
                 const tokenData = jwt.verify(req.headers.authorization.split(" ")[1], config.jwt_secret);
-                if (tokenData["discordId"]) {
-                    type = "discord";
-                    id = tokenData["discordId"];
-                } else {
-                    type = "website";
-                    id = tokenData["id"];
-                }
-            } catch (ignored) {}
+                type = "discord";
+                id = tokenData["discordId"];
+            } catch (e) {
+                id = "invalid token";
+            }
+        } else if (req.cookies["token"]) {
+            try {
+                const tokenData = jwt.verify(req.cookies["token"], config.jwt_secret);
+                type = "website";
+                id = tokenData["id"];
+            } catch (e) {
+                id = "invalid token";
+            }
         }
         console.log(`${new Date().toISOString()}: ${req.method} ${basePath + req.path} from ${req.ip} with ${JSON.stringify(req.body)} as ${id} ${type ? `(${type})` : ""}`);
         next();
