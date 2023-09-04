efe9c65c494872576e913deafd851350cf8da96a
XibalbaM <maelporret@outlook.fr>
2023-09-04 19:45:17 +0200

    fix(front): Add overlay for the modals

--- front/src/app/shared/components/modal/modal.component.ts
+++ front/src/app/shared/components/modal/modal.component.ts
@@ -9,5 +9,26 @@ export class ModalComponent {
 
     @Input() mtitle!: string;
     @Input() mtext!: string;
+    @Input() dialog!: HTMLDialogElement;
     @Output() close = new EventEmitter<void>();
+
+    ngOnInit() {
+        this.dialog.classList.add('z-50');
+        const show = this.dialog.show;
+        this.dialog.show = () => {
+            const overlay = document.createElement('div');
+            overlay.classList.add('z-40', 'bg-purple', 'opacity-20', 'absolute', 'top-0', 'start-0', 'w-screen', 'h-screen');
+            overlay.id = 'overlay';
+            document.body.appendChild(overlay);
+            show.call(this.dialog);
+        }
+        const close = this.dialog.close;
+        this.dialog.close = () => {
+            const overlay = document.getElementById('overlay');
+            if (overlay) {
+                overlay.remove();
+            }
+            close.call(this.dialog);
+        }
+    }
 }
