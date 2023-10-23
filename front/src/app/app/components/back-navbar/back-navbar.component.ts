5978419276900f5e9ff99f9a6cd9b1b7e6269da4
XibalbaM <maelporret@outlook.fr>
2023-10-23 18:13:12 +0200

    feat(front): Add a way to setup back-navbar.component using a service instead of router data

--- front/src/app/app/components/back-navbar/back-navbar.component.ts
+++ front/src/app/app/components/back-navbar/back-navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {Location} from "@angular/common";
 import {NAVBAR_DATA_TOKEN} from "../../app.component";
+import {BackNavbarService} from "../../services/back-navbar.service";
 
 @Component({
   selector: 'app-back-navbar',
@@ -13,4 +14,13 @@ export class BackNavbarComponent {
     backAmount = this.navbarData?.currentRoute.data["backAmount"] || 1;
     backUrl = this.navbarData?.currentRoute.data["navbarBackUrl"] || inject(Location).path().split('/').slice(0, -this.backAmount).join('/');
     text?: string = this.navbarData?.currentRoute.data["navbarText"];
+
+    constructor(private backNavbarService: BackNavbarService) {
+        if (this.backUrl === "service") {
+            this.backUrl = this.backNavbarService.backUrl || '/';
+        }
+        if (this.text === "service") {
+            this.text = this.backNavbarService.text;
+        }
+    }
 }
