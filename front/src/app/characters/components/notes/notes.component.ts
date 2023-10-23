1a4641681f201cf753b6510ea3f9cb2562a86590
XibalbaM <maelporret@outlook.fr>
2023-10-23 18:20:51 +0200

    feat(front): Add a markdown to notes.component

--- front/src/app/characters/components/notes/notes.component.ts
+++ front/src/app/characters/components/notes/notes.component.ts
@@ -8,6 +8,9 @@ import {IdToDataPipe} from "../../../shared/pipes/id-to-data.pipe";
 import {DataService} from "../../../app/services/data.service";
 import {CharacterService} from "../../services/character.service";
 import {NotificationService} from "../../../app/services/notification.service";
+import {BackNavbarService} from "../../../app/services/back-navbar.service";
+
+let noteTemp: Map<string, string | undefined> = new Map<string, string>();
 
 @Component({
     selector: 'app-notes',
@@ -21,14 +24,14 @@ export class NotesComponent implements OnInit {
 
     constructor(private auth: Auth, private route: ActivatedRoute, private title: Title,
                 private idToData: IdToDataPipe, private dataService: DataService, private router: Router,
-                private characterService: CharacterService, private notificationService: NotificationService) {
+                private characterService: CharacterService, private notificationService: NotificationService, private backNavbarService: BackNavbarService) {
     }
 
     ngOnInit() {
         combineLatest([this.route.paramMap, this.auth.userObservableOnceLoaded()]).subscribe(([params, user]) => {
             if (params.get('characterId') && user.characters.find((character) => character._id === params.get('characterId'))) {
                 this.character = (user.characters.find((character) => character._id === params.get('characterId'))!);
-                this.notes = this.character.notes;
+                this.notes = noteTemp.get(this.character._id) || this.character.notes;
                 this.title.setTitle(`${this.character.firstName} ${this.idToData.transform(this.character.clan, this.dataService.clans.getValue())?.name}, Notes — Fiche de personnage — Naruto jdr`);
             } else {
                 this.router.navigate(['/personnages']);
@@ -48,6 +51,14 @@ export class NotesComponent implements OnInit {
     }
 
     goBack() {
+        noteTemp.delete(this.character._id);
         this.router.navigate(['/personnages', this.character._id]);
+    }
+
+    goToFormatHelp() {
+        noteTemp.set(this.character._id, this.notes);
+        this.backNavbarService.backUrl = this.router.url;
+        this.backNavbarService.text = "Notes"
+        this.router.navigate(['/mise-en-forme']);
     }
 }
