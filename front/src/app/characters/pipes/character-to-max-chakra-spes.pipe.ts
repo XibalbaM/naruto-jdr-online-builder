1cc003ad551d349e0aeb7abe31b949ee2fd952b3
XibalbaM <maelporret@outlook.fr>
2023-10-28 17:56:27 +0200

    fix(front): Fix error making character-to-max-chakra-spes.pipe.ts return undefined for chakra control over 20

--- front/src/app/characters/pipes/character-to-max-chakra-spes.pipe.ts
+++ front/src/app/characters/pipes/character-to-max-chakra-spes.pipe.ts
@@ -8,36 +8,35 @@ import {map, Observable} from "rxjs";
 })
 export class CharacterToMaxChakraSpesPipe implements PipeTransform {
 
-    readonly chakraSpesPerChakraControl = {
-        2: 1,
-        3: 1,
-        4: 1,
-        5: 2,
-        6: 2,
-        7: 2,
-        8: 2,
-        9: 2,
-        10: 4,
-        11: 4,
-        12: 4,
-        13: 4,
-        14: 6,
-        15: 6,
-        16: 6,
-        17: 6,
-        18: 6,
-        19: 6,
-        20: 9,
-    }
-
     constructor(private characterToChakraControl: CharacterToChakraControlPipe) {}
 
     transform(character: Character): number
     transform(character: Observable<Character>): Observable<number>
     transform(character: Observable<Character> | Character): Observable<number> | number {
         if (character instanceof Character)
-            return this.chakraSpesPerChakraControl[this.characterToChakraControl.transform(character) as keyof typeof this.chakraSpesPerChakraControl];
+            return this.processCharacter(character);
         else
-            return character.pipe(map(character => this.chakraSpesPerChakraControl[this.characterToChakraControl.transform(character) as keyof typeof this.chakraSpesPerChakraControl]));
+            return character.pipe(map(character => this.processCharacter(character)));
+    }
+
+    processCharacter(character: Character): number {
+        const chakraControl = this.characterToChakraControl.transform(character);
+        let maxChakraSpes = 1;
+        if (chakraControl >= 5) {
+            maxChakraSpes += 1;
+        }
+        if (chakraControl >= 10) {
+            maxChakraSpes += 2;
+        }
+        if (chakraControl >= 14) {
+            maxChakraSpes += 2;
+        }
+        if (chakraControl >= 20) {
+            maxChakraSpes += 3;
+        }
+        if (chakraControl >= 24) {
+            maxChakraSpes += 5;
+        }
+        return maxChakraSpes;
     }
 }
