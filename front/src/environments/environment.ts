fa985bee4a9ca20fb7e58e3e9fc15e1bfddc2436
XibalbaM <maelporret@outlook.fr>
2023-12-30 16:47:04 +0100

    fix(front): Update recaptcha key

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,6 +3,6 @@ import Environment from "./environment.interface";
 export const environment: Environment = {
     api_url: 'https://builder.naruto-jdr.com/api',
     contactUrl: '',
-    recaptchaSiteKey: '6Leq7AcmAAAAAC4p3FeNSFr99uS7Ksn06YMMylt9',
+    recaptchaSiteKey: '6Lcx9UApAAAAANZ9RiqrFZ4Q-0Wn1ZWuzh3y7MVR',
     discordLogUrl: 'https://discord.com/api/oauth2/authorize?client_id=1126578016470958162&redirect_uri=https%3A%2F%2Fbuilder.naruto-jdr.com%2Fcompte%2Fdiscord&response_type=code&scope=identify'
 }
