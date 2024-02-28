2ba1f72d9275a83a14d824f41303ddabf938dece
xibalbam <maelporret@outlook.fr>
2024-02-28 22:31:00 +0100

    feat(front): Add service worker

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,27 +1,30 @@
 import {AppComponent} from './app/app/app.component';
-import {withInterceptorsFromDi, provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {appRoutes} from './app/app/app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
-import {BrowserModule, bootstrapApplication} from '@angular/platform-browser';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
 import {DataService} from './app/app/services/data.service';
 import {AuthService} from './app/app/services/auth.service';
-import {APP_INITIALIZER, importProvidersFrom} from '@angular/core';
+import {APP_INITIALIZER, importProvidersFrom, isDevMode} from '@angular/core';
 import {environment} from './environments/environment';
 import Environment from "./environments/environment.interface";
 import Auth from "./app/app/models/auth.model";
 import {provideRouter} from "@angular/router";
 import {NgxPopperjsModule} from "ngx-popperjs";
 import {pipes} from "./app/pipes";
+import {provideServiceWorker} from '@angular/service-worker';
 
 const init = (authService: AuthService, dataService: DataService) => {
-    authService.refreshUser();
-    dataService.init();
+    return () => {
+        authService.refreshUser();
+        dataService.init();
+    }
 };
 
 const dataProviders = [
     {provide: Environment, useValue: environment},
     {provide: Auth, useValue: new Auth()},
-    {provide: APP_INITIALIZER, useFactory: init, deps: [AuthService, DataService]},
+    {provide: APP_INITIALIZER, useFactory: init, deps: [AuthService, DataService], multi: true},
 ];
 
 bootstrapApplication(AppComponent, {
@@ -33,5 +36,9 @@ bootstrapApplication(AppComponent, {
         provideRouter(appRoutes),
         pipes,
         dataProviders,
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        })
     ]
 }).catch(err => console.error(err));
