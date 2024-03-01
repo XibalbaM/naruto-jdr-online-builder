76cf3be1bba8923b51c390694bd9d69656ab9c9c
xibalbam <maelporret@outlook.fr>
2024-03-01 23:21:50 +0100

    refactor(front): Migrate edit component to signals

--- front/src/app/characters/components/edit/edit.component.ts
+++ front/src/app/characters/components/edit/edit.component.ts
@@ -1,16 +1,15 @@
-import {AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, effect, OnInit, signal} from '@angular/core';
 import Character from "../../../app/models/character.model";
 import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import Auth from "../../../app/models/auth.model";
 import {DataService} from "../../../app/services/data.service";
 import {IdToDataPipe} from "../../../utils/pipes/id-to-data.pipe";
 import Environment from "../../../../environments/environment.interface";
-import {BehaviorSubject, combineLatest, every, merge, Observable, take} from "rxjs";
+import {combineLatest, every, merge, Observable, take, tap} from "rxjs";
 import {CharacterService} from "../../services/character.service";
 import {NotificationService} from "../../../app/services/notification.service";
 import Base from "../../../app/models/base.model";
 import {Title} from "@angular/platform-browser";
-import ChakraSpe from "../../../app/models/chakra-spe.model";
 import {CharacterToSkillReinforcementPipe} from '../../pipes/character-to-skill-reinforcement.pipe';
 import {CharacterToInterceptionsPipe} from '../../pipes/character-to-interceptions.pipe';
 import {CharacterToSkillTotalLevelPipe} from '../../pipes/character-to-skill-total-level.pipe';
@@ -39,35 +38,51 @@ import {NgxMarkdownItModule} from "ngx-markdown-it";
     templateUrl: './edit.component.html',
     styleUrls: ['./edit.component.scss'],
     standalone: true,
-    imports: [NgIf, NgxMarkdownItModule, RouterLink, ArrowRightComponent, NgClass, SpacerGraphicalComponent, NgFor, SpacerComponent, MinusSymbolComponent, PlusSymbolComponent, BgComponent, SkillItemComponent, AsyncPipe, IdToDataPipe, CharacterToReamingXpPipe, CharacterToMaxSkillCountPipe, CharacterToMaxChakraPipe, CharacterToMaxChakraSpesPipe, CharacterToChakraControlPipe, CharacterToBaseLevelPipe, CharacterToChakraSpeAmountPipe, CharacterToChakraRegenPipe, CharacterToSkillNaturalLevelPipe, CharacterToSkillTotalLevelPipe, CharacterToInterceptionsPipe, CharacterToSkillReinforcementPipe, JsonPipe]
+    imports: [NgIf, NgxMarkdownItModule, RouterLink, ArrowRightComponent, NgClass, SpacerGraphicalComponent, NgFor, SpacerComponent, MinusSymbolComponent, PlusSymbolComponent, BgComponent, SkillItemComponent, AsyncPipe, IdToDataPipe, CharacterToReamingXpPipe, CharacterToMaxSkillCountPipe, CharacterToMaxChakraPipe, CharacterToMaxChakraSpesPipe, CharacterToChakraControlPipe, CharacterToBaseLevelPipe, CharacterToChakraSpeAmountPipe, CharacterToChakraRegenPipe, CharacterToSkillNaturalLevelPipe, CharacterToSkillTotalLevelPipe, CharacterToInterceptionsPipe, CharacterToSkillReinforcementPipe, JsonPipe],
+    /*changeDetection: ChangeDetectionStrategy.OnPush*/
 })
-export class EditComponent implements OnInit, AfterViewInit, OnDestroy {
+export class EditComponent implements OnInit, AfterViewInit {
 
-    $character: BehaviorSubject<Character> = new BehaviorSubject<Character>(new Character());
-    commonSkills: { skill: Skill, level: number }[] = [];
-    uncommonSkills: { skill: CustomSkill, level: number }[] = [];
-    bases: { base: Base, level: number }[] = [];
-    shouldTruncNotes: boolean = false;
-    notes!: string;
-    characterChakraSpes: { chakraSpe: ChakraSpe, number: number }[] = [];
+    character = signal(new Character(), {equal: (a, b) => false});
+    commonSkills = computed(() =>
+        this.character().commonSkills.map((level, id) => ({skill: this.idToData.transform(id, this.dataService.commonSkills.getValue())!, level}))
+    );
+    uncommonSkills = computed(() =>
+        this.character().customSkills.map(skill => ({skill: this.idToData.transform(skill.skill, this.dataService.customSkills.getValue())!, level: skill.level}))
+    );
+    bases = computed(() =>
+        this.dataService.bases.getValue().map((base) => ({base, level: this.character().bases[base._id]}))
+    );
+    shouldTruncNotes = signal(false);
+    notes = computed(() => this.character().notes || "Pas encore de notes.");
+    chakraSpes = computed(() => {
+        let map = this.character().chakraSpes.reduce((acc: { [key: string]: number }, spe) => {
+            acc[spe] = (acc[spe] || 0) + 1;
+            return acc;
+        }, {});
+        return Object.entries(map).map(([chakraSpe, number]) => ({chakraSpe: this.idToData.transform(chakraSpe, this.dataService.chakraSpes.getValue())!, number}));
+    });
     protected readonly Math = Math;
 
     constructor(private activeRoute: ActivatedRoute, protected router: Router, protected auth: Auth,
                 protected dataService: DataService, private idToData: IdToDataPipe, private changeDetectorRef: ChangeDetectorRef,
                 protected env: Environment, private characterService: CharacterService, private notificationService: NotificationService,
                 private title: Title) {
+        effect(() => {
+            this.title.setTitle(`${this.character().firstName} ${this.idToData.transform(this.character().clan, this.dataService.clans.getValue())?.name}, Fiche de personnage — Naruto jdr`)
+        });
     }
 
     ngAfterViewInit() {
         const notes = document.getElementById('notes')!;
         const lineHeight = parseInt(window.getComputedStyle(notes).lineHeight);
         const maxHeight = lineHeight * 8;
-        this.shouldTruncNotes = notes.scrollHeight > maxHeight;
+        this.shouldTruncNotes.set(notes.scrollHeight > maxHeight);
         this.changeDetectorRef.detectChanges();
     }
 
     increaseBaseLevel(base: Base, currentLevel: number) {
-        if (currentLevel < this.idToData.transform(this.$character.getValue().rank, this.dataService.ranks.value)!.maxBase) {
+        if (currentLevel < this.idToData.transform(this.character().rank, this.dataService.ranks.value)!.maxBase) {
             this.setBaseLevel(base, currentLevel + 1);
         }
     }
@@ -84,50 +99,63 @@ export class EditComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     decreaseSkillLevelsIfNeeded(data: { base: Base, level: number }): Observable<boolean> {
-        const character = this.$character.getValue();
-        const commonSkillsToDecrease = character.commonSkills
+        const commonSkillsToDecrease = this.character().commonSkills
             .filter(level => level > 1)
             .map((level, id) => ({skill: this.idToData.transform(id, this.dataService.commonSkills.value)!, level}))
             .filter((skill: { skill: Skill, level: number }) => skill.skill.base === data.base._id)
             .filter((skill: { skill: Skill, level: number }) => skill.level >= data.level + 2);
-        const customSkillsToDecrease = character.customSkills
+        const customSkillsToDecrease = this.character().customSkills
             .filter(skill => skill.level > 1)
             .map((skill: { skill: string, level: number }) => ({skill: this.idToData.transform(skill.skill, this.dataService.customSkills.value)!, level: skill.level}))
             .filter((skill: { skill: Skill, level: number }) => skill.skill.base === data.base._id)
             .filter((skill: { skill: Skill, level: number }) => skill.level >= data.level + 2);
         const requests = merge(...[...commonSkillsToDecrease, ...customSkillsToDecrease].map((skill) => {
-            return this.characterService.setSkillLevel(character._id, skill.skill._id, skill.level - 1);
+            return this.characterService.setSkillLevel(this.character()._id, skill.skill._id, skill.level - 1);
         }))
         return requests.pipe(
-            every((success: boolean) => success)
+            every((success: boolean) => success),
+            tap((success) => {
+                if (success && (customSkillsToDecrease.length > 0 || commonSkillsToDecrease.length > 0)) {
+                    this.character.update(character => {
+                        commonSkillsToDecrease.forEach((skill) => {
+                            character.commonSkills[Number(skill.skill._id)] = skill.level - 1;
+                        });
+                        customSkillsToDecrease.forEach((skill) => {
+                            character.customSkills.find((data) => data.skill === skill.skill._id)!.level = skill.level - 1;
+                        });
+                        return character;
+                    });
+                }
+            })
         );
     }
 
     setSkillLevel(skill: Skill, level: number) {
-        if (level <= 0 || level > this.$character.getValue().bases[skill.base] + 2) {
+        if (level <= 0 || level > this.character().bases[skill.base] + 2) {
             return;
         }
-        this.characterService.setSkillLevel(this.$character.getValue()._id, skill._id, level).subscribe((success) => {
+        this.characterService.setSkillLevel(this.character()._id, skill._id, level).subscribe((success) => {
             if (success) {
-                const character = this.$character.getValue();
-                if (skill instanceof CustomSkill)
-                    character.customSkills.find((data) => data.skill === skill._id)!.level = level;
-                else
-                    character.commonSkills[Number(skill._id)] = level;
-                this.$character.next(character);
+                this.character.update(character => {
+                    if (skill instanceof CustomSkill)
+                        character.customSkills.find((data) => data.skill === skill._id)!.level = level;
+                    else
+                        character.commonSkills[Number(skill._id)] = level;
+                    return character;
+                });
             } else
                 this.notificationService.showNotification('Une erreur est survenue', 'Une erreur est survenue lors de la modification du niveau de la compétence, si le problème persiste, contactez nous');
         });
     }
 
     setBaseLevel(base: Base, level: number) {
-        this.characterService.setBaseLevel(this.$character.getValue()._id, base._id, level).subscribe((success) => {
+        this.characterService.setBaseLevel(this.character()._id, base._id, level).subscribe((success) => {
             if (success) {
-                let character = this.$character.getValue();
-                this.bases.find((data) => data.base._id === base._id)!.level = level;
-                character.bases = this.bases.map((base) => base.level);
-                this.$character.next(character);
-                this.auth.user!.characters.find((character) => character._id === character._id)!.bases = this.bases.map((base) => base.level);
+                this.character.update(character => {
+                    character.bases[base._id] = level;
+                    return character;
+                })
+                this.auth.user!.characters.find((character) => character._id === character._id)!.bases = this.character().bases;
                 this.auth.userObservable().next(this.auth.user);
             } else
                 this.notificationService.showNotification('Une erreur est survenue', 'Une erreur est survenue lors de la modification du niveau de la base, si le problème persiste, contactez nous');
@@ -138,18 +166,16 @@ export class EditComponent implements OnInit, AfterViewInit, OnDestroy {
         if (nindo < 0) {
             return;
         }
-        this.characterService.setNindoPoints(this.$character.getValue()._id, nindo).subscribe((success) => {
+        this.characterService.setNindoPoints(this.character()._id, nindo).subscribe((success) => {
             if (success) {
-                const character = this.$character.getValue();
-                character.nindoPoints = nindo;
-                this.$character.next(character);
+                this.character.update(character => {character.nindoPoints = nindo; return character;});
             } else
                 this.notificationService.showNotification('Une erreur est survenue', 'Une erreur est survenue lors de la modification du nombre de points de nindo, si le problème persiste, contactez nous');
         });
     }
 
     getBaseLevelBySkill(skill: Skill) {
-        return this.bases.find((base) => base.base._id === skill.base)!.level;
+        return this.bases().find((base) => base.base._id === skill.base)!.level;
     }
 
     canBaseLevelReduced(data: { base: Base, level: number }): boolean {
@@ -159,30 +185,10 @@ export class EditComponent implements OnInit, AfterViewInit, OnDestroy {
     ngOnInit() {
         combineLatest([this.activeRoute.paramMap, this.auth.userObservableOnceLoaded()]).pipe(take(1)).subscribe(([params, user]) => {
             if (params.get('characterId') && user?.characters.find((character: Character) => character._id === params.get('characterId'))) {
-                this.$character.subscribe((character) => {
-                    this.commonSkills = character.commonSkills.map((level, id) => ({skill: this.idToData.transform(id, this.dataService.commonSkills.getValue())!, level}));
-                    this.uncommonSkills = character.customSkills.map(skill => ({skill: this.idToData.transform(skill.skill, this.dataService.customSkills.getValue())!, level: skill.level}));
-                });
-                this.$character.next(user?.characters.find((character: Character) => character._id === params.get('characterId'))!);
-                this.bases = this.dataService.bases.getValue().map((base) => ({base, level: this.$character.getValue().bases[base._id]}));
-                this.notes = this.$character.value.notes || "Pas encore de notes.";
-                this.$character.getValue().chakraSpes.forEach((chakraSpe) => {
-                    const chakraSpeData = this.characterChakraSpes.slice().sort((a, b) => b.number - a.number).find(spe => spe.chakraSpe._id === chakraSpe);
-                    if (chakraSpeData) {
-                        this.characterChakraSpes.push({chakraSpe: chakraSpeData.chakraSpe, number: chakraSpeData.number + 1});
-                    } else {
-                        this.characterChakraSpes.push({chakraSpe: this.idToData.transform(chakraSpe, this.dataService.chakraSpes.value)!, number: 1});
-                    }
-                });
-                this.title.setTitle(`${this.$character.getValue().firstName} ${this.idToData.transform(this.$character.getValue().clan, this.dataService.clans.getValue())?.name}, Fiche de personnage — Naruto jdr`)
+                this.character.set(user?.characters.find((character: Character) => character._id === params.get('characterId'))!);
             } else {
                 this.router.navigate(['/personnages']);
             }
         });
-    }
-
-    ngOnDestroy() {
-        let find = this.auth.user!.characters.find((c) => c._id === this.$character.getValue()._id);
-        if (find) find.bases = this.bases.map((base) => base.level);
     }
 }
