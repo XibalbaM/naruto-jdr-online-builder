59e19293d9ff53359b55078d42dd1fccb1d529f3
xibalbam <maelporret@outlook.fr>
2024-06-01 17:06:47 +0200

    fix(bot): Set right api url

--- bot/src/config/env/prod.env.ts
+++ bot/src/config/env/prod.env.ts
@@ -4,7 +4,7 @@ export default {
     env: "prod",
     token: process.env.DISCORD_TOKEN,
     clientId: process.env.DISCORD_CLIENT_ID,
-    api_url: "http://localhost:3000/api/discord",
+    api_url: "https://ninjadex.naruto-jdr.com/api/discord",
     jwt_secret: process.env.JWT_SECRET,
     jwt_expiration: 60,
 } as Environment;
