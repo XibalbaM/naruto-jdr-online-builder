42a5370f3a0c9ca4cac06cff9a1fb44bd6a85a11
xibalbam <maelporret@outlook.fr>
2024-07-13 12:16:43 +0200

    fix(front): Fix skillTypeToTypeName pipe witch was always giving "Compétence Commune"

--- front/src/app/characters/pipes/skill-type-to-type-name.pipe.ts
+++ front/src/app/characters/pipes/skill-type-to-type-name.pipe.ts
@@ -8,19 +8,15 @@ import {CustomSkill, Skill} from "../../app/models/skill.model";
 export class SkillToTypeNamePipe implements PipeTransform {
 
     transform(value: Skill): string {
-        if (value instanceof CustomSkill) {
-            switch (value.type) {
-                case "combat":
-                    return "Compétence de combat";
-                case "terrain":
-                    return "Compétence de terrain";
-                case "clan":
-                    return "Compétence de clan";
-                default:
-                    return "";
-            }
-        } else {
-            return "Compétence commune";
+        switch ((value as CustomSkill).type) {
+            case "combat":
+                return "Compétence de combat";
+            case "terrain":
+                return "Compétence de terrain";
+            case "clan":
+                return "Compétence de clan";
+            default:
+                return "Compétence commune";
         }
     }
 }
