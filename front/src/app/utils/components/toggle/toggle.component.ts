54c284dd101c2187176baeab9af708c87db55428
xibalbam <maelporret@outlook.fr>
2024-10-22 00:05:27 +0200

    feat(front): Add a toggle component

---
+++ front/src/app/utils/components/toggle/toggle.component.ts
@@ -0,0 +1,25 @@
+import {Component, effect, EventEmitter, Input, Output, signal} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-toggle',
+  standalone: true,
+    imports: [
+        FormsModule,
+        NgIf,
+        NgOptimizedImage
+    ],
+  templateUrl: './toggle.component.html',
+  styleUrl: './toggle.component.scss'
+})
+export class ToggleComponent {
+
+    @Input() value = false;
+    @Output() valueChange = new EventEmitter<boolean>();
+
+    toggle() {
+        this.value = !this.value;
+        this.valueChange.emit(this.value);
+    }
+}
