000c7e483cd95040ce6c67369f0d5f9dc2568ee0
xibalbam <maelporret@outlook.fr>
2024-10-22 00:06:45 +0200

    refactor(front): Use the new toggle component

--- front/src/app/admin/components/home/home.component.ts
+++ front/src/app/admin/components/home/home.component.ts
@@ -8,13 +8,14 @@ import {SpacerComponent} from '../../../utils/components/spacer/spacer.component
 import {AdminLogoComponent} from '../../../utils/components/admin-logo/admin-logo.component';
 import {Router} from "@angular/router";
 import AdminService from "../../services/admin.service";
+import {ToggleComponent} from "../../../utils/components/toggle/toggle.component";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
     standalone: true,
-    imports: [AdminLogoComponent, SpacerComponent, HomeButtonComponent, FormsModule, NgIf, NgOptimizedImage]
+    imports: [AdminLogoComponent, SpacerComponent, HomeButtonComponent, FormsModule, NgIf, NgOptimizedImage, ToggleComponent]
 })
 export class HomeComponent implements OnInit {
 
