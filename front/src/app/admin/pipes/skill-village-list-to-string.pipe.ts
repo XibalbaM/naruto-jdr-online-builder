3021bd33acd3d6a81f914a5e3202893f255dfdda
xibalbam <maelporret@outlook.fr>
2024-10-22 00:06:17 +0200

    fix(front): Make skill-village-list-to-string.pipe.ts convert id to name

--- front/src/app/admin/pipes/skill-village-list-to-string.pipe.ts
+++ front/src/app/admin/pipes/skill-village-list-to-string.pipe.ts
@@ -1,6 +1,8 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import Skill from "naruto-jdr-online-builder-common/src/interfaces/skill.interface";
 import CustomSkill from "../../app/models/skill.interface";
+import {IdToDataPipe} from "../../utils/pipes/id-to-data.pipe";
+import {DataService} from "../../app/services/data.service";
 
 @Pipe({
     name: 'skillVillageListToString',
@@ -8,10 +10,13 @@ import CustomSkill from "../../app/models/skill.interface";
 })
 export class SkillVillageListToStringPipe implements PipeTransform {
 
+    constructor(private idToDataPipe: IdToDataPipe, private dataService: DataService) {
+    }
+
     transform(value: Skill | CustomSkill): String {
         if (!value.villages || value.villages.length === 0) {
             return "Par défaut";
         }
-        return value.villages.join(', ');
+        return value.villages.map(village => this.idToDataPipe.transform(village, this.dataService.villages)?.name).join(', ');
     }
 }
