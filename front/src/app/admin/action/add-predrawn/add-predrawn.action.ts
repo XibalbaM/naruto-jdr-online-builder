bde5c8fd340e1b5bda57ff1f36342913ce062795
xibalbam <maelporret@outlook.fr>
2024-12-04 22:48:27 +0100

    fix(front): Fix character list not updating on new predrawn

--- front/src/app/admin/action/add-predrawn/add-predrawn.action.ts
+++ front/src/app/admin/action/add-predrawn/add-predrawn.action.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {NotificationService} from "../../../app/services/notification.service";
 import {take} from "rxjs";
 import {PredrawnService} from "../../../app/services/predrawn.service";
+import Auth from "../../../app/models/auth.model";
 
 @Component({
   template: '',
@@ -11,19 +12,23 @@ import {PredrawnService} from "../../../app/services/predrawn.service";
 export class AddPredrawnAction implements OnInit {
 
     constructor(private activeRoute: ActivatedRoute, private router: Router, private notificationService: NotificationService,
-                private predrawnService: PredrawnService) {
+                private predrawnService: PredrawnService, private auth: Auth) {
     }
 
     ngOnInit() {
         this.activeRoute.paramMap.pipe(take(1)).subscribe((params) => {
             this.predrawnService.addPredrawnCharacter(params.get('predrawnId')!).subscribe(({success, id}) => {
                 if (success) {//TODO update cache
+                    this.auth.user?.characters.push({
+                        ...this.auth.user?.characters.find((character) => character._id === params.get('predrawnId'))!,
+                        _id: id!
+                    });
                     this.notificationService.showNotification("Succès", "Le personnage a été ajouté aux prétirés avec succès.");
+                    this.router.navigateByUrl("/admin/pretires");
                 } else {
                     this.notificationService.showNotification("Erreur", "Une erreur est survenue lors de l'ajout du personnage aux prétirés.");
                 }
             });
-            this.router.navigateByUrl("/admin/pretires");
         });
     }
 }
